eb837608334a2d9b9781c5ab2e731dbe4fd7b3c9
jbeltre <joel.beltre@lastminute.com>
2020-07-10 17:57:06 +0200

    changes to eslintrc so it doesnt give error on missing ts and tsx extensions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   extends: "airbnb",
   rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }]
-  }
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx", ".json"] }],
+    'import/extensions': 'off'
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    },
+  },
 };
