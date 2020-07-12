d0fd193a007c029e0a9c47f95847cc1494e567e1
jbeltre <joel.beltre@lastminute.com>
2020-07-12 10:18:37 +0200

    Value to ChunkValue

---
+++ src/url/partials/ChunkValue.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+const focusChunk = (ref) => {
+  if (ref && ref.current) ref.current.focus();
+};
+
+const ChunkValue = ({
+  partValue, isAdditionalValue, partId, valueId, updateAdditionalValue,
+}) => {
+  const chunkInputRef = useRef(null);
+
+  useEffect(() => {
+    focusChunk(chunkInputRef);
+  }, []);
+
+  return (
+    isAdditionalValue ? (
+      <input
+        type="text"
+        value={partValue}
+        placeholder="Add value"
+        onChange={e => updateAdditionalValue(e.target.value, partId, valueId)}
+        ref={chunkInputRef}
+      />
+    ) : (
+      <div className="url-part__value">
+        {partValue}
+      </div>
+    )
+  );
+};
+
+export default ChunkValue;
+
+ChunkValue.propTypes = {
+  updateAdditionalValue: PropTypes.func.isRequired,
+  isAdditionalValue: PropTypes.bool,
+  partId: PropTypes.number.isRequired,
+  valueId: PropTypes.number.isRequired,
+  partValue: PropTypes.string,
+};
+
+ChunkValue.defaultProps = {
+  isAdditionalValue: false,
+  partValue: '',
+};
