6f9434a6f198b187e4ae6f7d45566fbf05c57765
jbeltre <joel.beltre@lastminute.com>
2020-07-31 23:52:31 +0200

    styles cleanup

--- src/url/UrlComponent.tsx
+++ src/url/UrlComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import UrlChunks from './urlChunk/UrlChunks';
 import SaveUrlButton from './partials/SaveUrlButton';
+import './Url.scss';
 
 type Props = {
   chunks: Chunks,
--- src/url/urlChunk/UrlChunk.tsx
+++ src/url/urlChunk/UrlChunk.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AddButton from "../buttons/AddButton";
 import AdditionalChunkRow from "./AdditionalChunkRow";
+import './UrlChunk.scss';
 
 type Props = {
   chunkClass: string;
