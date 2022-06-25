0370243049daca20528549cf48362ec33f138ed9
jbeltre <joel.beltre@lastminute.com>
2022-06-25 14:34:26 +0200

    update manifest.json and favicon.ico

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,14 @@ import {Html, Head, Main, NextScript} from 'next/document'
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+                <meta name="theme-color" content="#000000"/>
+
+                <link rel="manifest" href="/manifest.json"/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
+            </Head>
             <body>
             <Main/>
             <NextScript/>
