58f646e018d4093964891b6d71e545ccdb1e72a8
jbeltre <joel.beltre@lastminute.com>
2022-06-26 09:37:49 +0200

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,21 +3,9 @@ import {initializeApp, getApps, getApp} from "firebase/app";
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import {useAuthState} from "react-firebase-hooks/auth";
 import Home from "../home/Home";
+import { initFirebaseApp } from "../utils";
 
-if (getApps().length === 0) {
-    
-    initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_APP_ID
-    })
-} else {
-    getApp();
-}
-
+initFirebaseApp();
 const auth = getAuth();
 
 const signInWithGoogle = () => {
